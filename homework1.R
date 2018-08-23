ce59813f8cb13c3d5d46e2de5bbf9e66f2fe9de9
Brian Rivers <brian.j.rivers.eit@gmail.com>
2018-08-23 06:30:17 -0400

    still working got it to import

--- homework1.R
+++ homework1.R
@@ -1,2 +1,16 @@
-a=1
-b=3
+#import data from file
+
+hwk1 <- read.csv(("C:/Users/bjriv/OneDrive - Georgia Institute of Technology/ISYE6501/credit_card_data-headers.txt"), header = TRUE)
+
+#print first line
+head(hwk1)
+
+#check results
+hwk1
+
+#look at data
+library(ggvis)
+
+#scatter plot
+
+hwk1 %>% ggvis(A2,A3)
